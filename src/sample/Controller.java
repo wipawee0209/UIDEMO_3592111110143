d0fbf71f2ed68c344aa373d26b7f9fb08074736a
wipawee0209 <rosewhite180209@gmail.com>
2018-02-23 12:03:57 +0700

    Connected to DB

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,17 +1,26 @@
 package sample;
 
+import admin.adminController;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
-    private loginModel loginModel = new loginModel();
+public class Controller implements Initializable{
+    private loginModel loginmodel = new loginModel();
+
     @FXML
     private JFXTextField username;
 
@@ -19,7 +28,7 @@ public class Controller implements Initializable {
     private JFXPasswordField password;
 
     @FXML
-    private JFXButton btnLogin;
+    private JFXButton btnlogin;
 
     @FXML
     private Label loginStatus;
@@ -30,12 +39,46 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        if (this.loginModel.isDatabaseConnection()){
-            this.dbStatus.setText("Connected to DB");
-        }else {
-            this.dbStatus.setText("Not Connected to DB");
+
+        if (this.loginmodel.isDatabaseConnection()){
+            this.dbStatus.setText("Connected to DB.");
+
+        }else{
+            this.dbStatus.setText("Not Connect to DB.");
+        }
+    }//initialii
+    @FXML
+    public  void  Login(ActionEvent event){
+        try{
+            if (this.loginmodel.isLogin(username.getText(),password.getText())){
+                Stage stage = (Stage) this.btnlogin.getScene().getWindow();
+                stage.close();
+                adminDashborad();
+
+            }else {
+                loginStatus.setText("Your username or password is in");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }//isLonig
+
+    private void adminDashborad() {
+        try{
+            Stage adminStage = new Stage();
+            FXMLLoader adminLoader = new FXMLLoader();
+            Pane adminRoot = (Pane) adminLoader.load(getClass().getResource("/admin/adminDashboard.fxml").openStream());
+            adminController admincontroller = adminLoader.getController();
+            Scene scene = new Scene(adminRoot);
+            adminStage.setScene(scene);
+            adminStage.setTitle("Admin Dashborad");
+            adminStage.setResizable(false);
+            adminStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
 
-    }
+    }//adminDashborad
 }//class
--- src/sample/loginModel.java
+++ src/sample/loginModel.java
@@ -1,28 +1,52 @@
 package sample;
 
-import java.sql.Connection;
-import java.sql.SQLException;
 import dbUtil.dbConnection;
 
-public class loginModel {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class loginModel {
     Connection connection;
 
-    public loginModel(){
+    public loginModel() {
         try {
             this.connection = dbConnection.getConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        if (this.connection == null) {
+        if (this.connection == null){
             System.exit(1);
         }
+
     }//loginModel
+    public  boolean isDatabaseConnection(){
+        return  this.connection !=null;
+    }
+    public  boolean isLogin (String user, String pass) throws SQLException {
+        PreparedStatement pr = null;
+        ResultSet rs = null;
 
-    public boolean isDatabaseConnection(){
-        return this.connection != null;
-    }//isDatabaseConnection
+        String sql = "select * from user where username =? and password = ?";
+        try {
+            pr = this.connection.prepareStatement(sql);
+            pr.setString(1,user);
+            pr.setString(2,pass);
+            rs = pr.executeQuery();
+            if (rs.next()){
+
+                return  true;
+            }
+            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }finally {
+            pr.close();
+            rs.close();
+        }
 
 
-
-}//calss
+    }//login
+}//class
