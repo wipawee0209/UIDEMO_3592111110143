bfa904d8da5046888b4012aa72a281e1830b0e7c
LAB_COM-MT_1 <rosewhite180209@gmail.com>
2018-02-25 17:14:34 +0700

    Save Student Data form Database

--- src/admin/adminController.java
+++ src/admin/adminController.java
@@ -208,6 +208,28 @@ public class adminController implements Initializable{
         }
 
     }//editStudent
+    @FXML
+    private void saveStudent(ActionEvent event){
+        StudentData std = studentTable.getSelectionModel().getSelectedItem();
+        //DialogMessage คำเตือนต้องการลบหรื่อไม่
+        JOptionPane.showConfirmDialog(null,"Do you want to update student name: "
+                +std.getId());
+        String sqlUpdate = "update student set firstName =?,"+"lastName =?,email =?,DOB =? where ID =?";
+        try {
+            Connection conn = dbConnection.getConnection();
+            PreparedStatement pr = conn.prepareStatement(sqlUpdate);
+            pr.setString(1,this.txtFristName.getText());
+            pr.setString(2,this.txtLastName.getText());
+            pr.setString(3,this.txtEmail.getText());
+            pr.setString(4,this.txtDOB.getEditor().getText());
+            pr.setString(5,this.txtID.getText());
 
+            pr.executeUpdate();
+            pr.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        loadStudentData(new ActionEvent());
+    }//saveStudent
 
 }//class
